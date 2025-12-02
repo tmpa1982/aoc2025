fbed5faf5b08cf1b8c4a131219bf6d20d2e9c809
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-12-02 08:37:50 +0800

    Day 1: parse rotation

---
+++ day1/rotation.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+from direction import Direction
+
+@dataclass
+class Rotation:
+    direction: Direction
+    size: int
+
+    @staticmethod
+    def parse(s: str):
+        if not s:
+            raise ValueError("Value is empty")
+        direction = Rotation.__parse_direction(s[0])
+        size = Rotation.__parse_size(s[1:])
+        return Rotation(direction, size)
+
+    @staticmethod
+    def __parse_direction(s: str):
+        match s:            
+            case "L": return Direction.LEFT
+            case "R": return Direction.RIGHT
+            case _: raise ValueError(f"Invalid direction: {s}")
+    
+    @staticmethod
+    def __parse_size(s: str):
+        try:
+            return int(s)
+        except:
+            raise ValueError(f"Invalid size: {s}")
---
+++ day1/test_rotation_parse.py
@@ -0,0 +1,28 @@
+import pytest
+
+from direction import Direction
+from rotation import Rotation
+
+def test_parse_left():
+    result = Rotation.parse("L7")
+    assert result == Rotation(Direction.LEFT, 7)
+
+def test_parse_right():
+    result = Rotation.parse("R5")
+    assert result == Rotation(Direction.RIGHT, 5)
+
+def test_invalid_direction():
+    with pytest.raises(ValueError, match="Invalid direction: A"):
+        Rotation.parse("A1")
+
+def test_invalid_size():
+    with pytest.raises(ValueError, match="Invalid size: x"):
+        Rotation.parse("Rx")
+
+def test_empty_value():
+    with pytest.raises(ValueError, match="Value is empty"):
+        Rotation.parse("")
+
+def test_empty_size():
+    with pytest.raises(ValueError, match="Invalid size: "):
+        Rotation.parse("R")
