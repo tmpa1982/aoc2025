5ea9197d82daf2f95b8d766c6430d78f30ad41e7
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-12-03 20:51:53 +0800

    Day 2: simplify range creation

--- day2/range.py
+++ day2/range.py
@@ -3,11 +3,9 @@ from dataclasses import dataclass
 
 @dataclass
 class Range():
-    start_str: str
-    end_str: str
     start_num: int
     end_num: int
 
     @staticmethod
     def create(start: str, end: str):
-        return Range(start, end, int(start), int(end))
+        return Range(int(start), int(end))
