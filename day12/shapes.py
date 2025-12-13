2e444f8cac5c94daa924fb9e478e336252b1a21f
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-12-13 22:36:49 +0800

    Day 12: hardcode shapes

---
+++ day12/shapes.py
@@ -0,0 +1,35 @@
+shape0 = [
+    [False, True, True],
+    [True, True, False],
+    [True, True, True],
+]
+
+shape1 = [
+    [False, False, True],
+    [False, True, True],
+    [True, True, True],
+]
+
+shape2 = [
+    [True, True, True],
+    [True, True, True],
+    [True, False, False],
+]
+
+shape3 = [
+    [True, True, True],
+    [False, True, False],
+    [True, True, True],
+]
+
+shape4 = [
+    [False, True, True],
+    [True, True, False],
+    [True, False, False],
+]
+
+shape5 = [
+    [True, True, True],
+    [True, False, False],
+    [True, True, True],
+]
